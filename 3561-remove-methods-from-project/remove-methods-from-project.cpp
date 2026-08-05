81cf2682eba658d24b588cfdeed8c0c887ef4278
Jasir-Ubaid <jasirubaid11@gmail.com>
2026-08-05 18:09:07 +0530

    Time: 21 ms (99.31%) | Memory: 254.6 MB (99.31%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.cpp
+++ 3561-remove-methods-from-project/remove-methods-from-project.cpp
@@ -1,31 +1,50 @@
+constexpr int N=1e5, M=2e5;
+struct Edge{ int v, nxt=-1; };
+Edge E[M];
+int eIdx=0;
+int adj[N];// heads of linked lists
+inline void addEdge(int u, int v){
+    E[eIdx]={v, adj[u]};
+    adj[u]=eIdx++;
+}
+uint8_t vis[N];
 class Solution {
 public:
-    vector<int> remainingMethods(int n,int k,vector<vector<int>>& invocations){
-        vector<vector<int>>g(n);
-        vector<int>vis(n);
-        for(auto &e:invocations) g[e[0]].push_back(e[1]);
-        queue<int>q;
-        q.push(k);
-        vis[k]=1;
-        while(!q.empty()){
-            int u=q.front();
-            q.pop();
-            for(int v:g[u]){
-                if(!vis[v]){
-                    vis[v]=1;
-                    q.push(v);
-                }
+    static void dfs(int u){
+        vis[u]=1;
+        for(int idx=adj[u]; idx!=-1; idx=E[idx].nxt){
+            const int v=E[idx].v;
+            if (vis[v]!=0) continue;
+            dfs(v);
+        }
+    }
+    static vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        eIdx=0;
+        memset(adj, -1, n*sizeof(int));
+        memset(vis, 0, n);
+        for(auto& e : invocations){
+            const int u=e[0], v=e[1];
+            addEdge(u, v);
+        }
+        dfs(k);
+        bool cnnt1=0;
+        for(auto& e : invocations){
+            const int u=e[0], v=e[1];
+            if ((vis[u]==0) & (vis[v]==1)){
+                cnnt1=1;
+                break;
             }
         }
-        for(auto &e:invocations){
-            if(!vis[e[0]] && vis[e[1]]){
-                vector<int>ans;
-                for(int i=0;i<n;i++)ans.push_back(i);
-                return ans;
-            }
+        vector<int> ans;
+        if (cnnt1){
+            ans.resize(n);
+            iota(ans.begin(), ans.end(), 0);
+            return ans;
         }
-        vector<int>ans;
-        for(int i=0;i<n;i++)if(!vis[i]) ans.push_back(i);
+        ans.reserve(n);
+        for(int i=0; i<n; i++){
+            if (vis[i]==0) ans.push_back(i);
+        }
         return ans;
     }
 };
